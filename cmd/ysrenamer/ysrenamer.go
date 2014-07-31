074f59d7e9b6fa3efec71b8a4c492a145ab48678
yuukichi <yuukichi41@gmail.com>
2014-07-31 19:06:17 +0000

    fixes #1. update cmd/ysrenamer/ysrenamer.go

--- cmd/ysrenamer/ysrenamer.go
+++ cmd/ysrenamer/ysrenamer.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "os"
+  "path/filepath"
 
   "github.com/yuukichi/ysrenamer/act"
 )
@@ -16,9 +17,16 @@ func main() {
   files := os.Args[2:]
   dests := act.ProcOrder(os.Args[1], files)
  
-  var dest string
   for i, src := range files {
-    dest = dests[i]
+    dest := dests[i]
+    dir := filepath.Dir(dest)
+    if _, err := os.Stat(dir) ; os.IsNotExist(err) {
+      if err := os.MkdirAll(dir, 0755) ; err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+      }
+    }
+
     if err := os.Rename(src, dest) ; err != nil {
       fmt.Println(err.Error())
       os.Exit(1)
